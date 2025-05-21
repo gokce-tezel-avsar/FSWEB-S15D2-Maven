b820fc7a5e855f429cdeceea27a9a7f636ad244d
gokce-tezel-avsar <gokcetezelavsar@gmail.com>
2025-05-22 00:23:57 +0300

    .

---
+++ src/main/java/org/example/entity/TaskData.java
@@ -0,0 +1,34 @@
+package org.example.entity;
+
+import java.util.Set;
+
+public class TaskData {
+    private Set<Task> annsTasks;
+    private Set<Task> bobsTasks;
+    private Set<Task> carolsTasks;
+    private Set<Task> unassignedTasks;
+
+
+    public TaskData(Set<Task> annsTasks, Set<Task> bobsTasks, Set<Task> carolsTasks, Set<Task> unassignedTasks) {
+        this.annsTasks = annsTasks;
+        this.bobsTasks = bobsTasks;
+        this.carolsTasks = carolsTasks;
+        this.unassignedTasks = unassignedTasks;
+    }
+
+    public  Set<Task> getTasks(String name) {
+        switch (name) {
+            case "ann":
+                return this.annsTasks;
+            case "bob":
+                return this.bobsTasks;
+            case "carol":
+                return this.carolsTasks;
+            case "all":
+                return getUnion(annsTasks, bobsTasks, carolsTasks);
+        }
+
+    }
+
+
+}
